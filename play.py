6c33e87072bef3bd10eb46cee43d2c9f9c392491
andrercardoso <andrericcardo1@gmail.com>
2022-03-14 18:00:36 +0100

    Update play.py

--- play.py
+++ play.py
@@ -4,7 +4,7 @@ from humanPlayer import HumanPlayer
 
 # Computer player Vs Human player
 pl1 = ComputerPlayer("computer", e_greedy_rate=0)
-pl1.load_knowledge('Policy_50,000_games_against_static_player')
+pl1.load_knowledge('Policy_100,000_games_against_computer_player')
 pl3 = HumanPlayer("Human")
 game = TicTacToe(pl1, pl3)
 game.play(verbose=True)
